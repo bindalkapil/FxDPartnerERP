af0311f5c4fe0ad5e3fae07f3773104d07dc5083
bindalkapil <kapil.bindal@vegrow.in>
2025-06-12 12:00:08 +0530

    deployemnt update

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/FxDPartnerERP/',
   build: {
     outDir: 'build',
-  },  // <-- Added this comma
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
